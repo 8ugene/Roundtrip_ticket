bd0011182bf574f362a447c40ba3e5da3f0dbf09
8ugene <54076386+8ugene@users.noreply.github.com>
2019-09-13 18:07:52 +0300

    Create Roundtrip_ticket.c

---
+++ Roundtrip_ticket.c
@@ -0,0 +1,161 @@
+vuser_init()
+{
+	#include "Login.h"
+	
+	return 0;
+}
+
+Action()
+{
+
+		
+	lr_think_time(10);
+
+	lr_start_transaction("1_transaction");
+
+	web_url("welcome.pl", 
+		"URL=http://127.0.0.1:1080/cgi-bin/welcome.pl?page=search", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://127.0.0.1:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Snapshot=t3.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_think_time(33); 
+	
+	web_reg_save_param_ex(
+		"ParamName=ID",
+		"LB=name=\"outboundFlight\" value=\"",
+		"RB=\"",
+		SEARCH_FILTERS,
+		LAST);
+	
+		
+	web_reg_save_param_ex(
+		"ParamName=ID2",
+		"LB=name=\"returnFlight\" value=\"",
+		"RB=\"",
+		SEARCH_FILTERS,
+		LAST);
+
+	web_submit_data("reservations.pl",
+		"Action=http://127.0.0.1:1080/cgi-bin/reservations.pl",
+		"Method=POST",
+		"TargetFrame=",
+		"RecContentType=text/html",
+		"Referer=http://127.0.0.1:1080/cgi-bin/reservations.pl?page=welcome",
+		"Snapshot=t4.inf",
+		"Mode=HTML",
+		ITEMDATA,
+		"Name=advanceDiscount", "Value=0", ENDITEM,
+		"Name=depart", "Value={NewParam_Depart}", ENDITEM,
+		"Name=departDate", "Value=08/03/2019", ENDITEM,
+		"Name=arrive", "Value={NewParam_arrive}", ENDITEM,
+		"Name=returnDate", "Value=08/04/2019", ENDITEM,
+		"Name=numPassengers", "Value=1", ENDITEM,
+		"Name=roundtrip", "Value=on", ENDITEM,
+		"Name=seatPref", "Value={NewParam}", ENDITEM,
+		"Name=seatType", "Value={ParamSet_1}", ENDITEM,
+		"Name=.cgifields", "Value=roundtrip", ENDITEM,
+		"Name=.cgifields", "Value=seatType", ENDITEM,
+		"Name=.cgifields", "Value=seatPref", ENDITEM,
+		"Name=findFlights.x", "Value=54", ENDITEM,
+		"Name=findFlights.y", "Value=8", ENDITEM,
+		LAST);
+
+	lr_end_transaction("1_transaction",LR_AUTO);
+
+	lr_think_time(14);
+
+	lr_start_transaction("2_transaction");
+
+	web_submit_data("reservations.pl_2", 
+		"Action=http://127.0.0.1:1080/cgi-bin/reservations.pl", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"RecContentType=text/html", 
+		"Referer=http://127.0.0.1:1080/cgi-bin/reservations.pl", 
+		"Snapshot=t5.inf", 
+		"Mode=HTML", 
+		ITEMDATA, 
+		"Name=outboundFlight", "Value={ID}", ENDITEM, 
+		"Name=returnFlight", "Value={ID2}", ENDITEM, 
+		"Name=numPassengers", "Value=1", ENDITEM, 
+		"Name=advanceDiscount", "Value=0", ENDITEM, 
+		"Name=seatType", "Value={ParamSet_1}", ENDITEM, 
+		"Name=seatPref", "Value={NewParam}", ENDITEM, 
+		"Name=reserveFlights.x", "Value=67", ENDITEM, 
+		"Name=reserveFlights.y", "Value=7", ENDITEM, 
+		LAST);
+
+	lr_think_time(18);
+
+	web_submit_data("reservations.pl_3", 
+		"Action=http://127.0.0.1:1080/cgi-bin/reservations.pl", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"RecContentType=text/html", 
+		"Referer=http://127.0.0.1:1080/cgi-bin/reservations.pl", 
+		"Snapshot=t6.inf", 
+		"Mode=HTML", 
+		ITEMDATA, 
+		"Name=firstName", "Value=name1", ENDITEM, 
+		"Name=lastName", "Value=name2", ENDITEM, 
+		"Name=address1", "Value=prost69", ENDITEM, 
+		"Name=address2", "Value=vrn/leo/398008", ENDITEM, 
+		"Name=pass1", "Value=name1 name2", ENDITEM, 
+		"Name=creditCard", "Value=12345678", ENDITEM, 
+		"Name=expDate", "Value=06/19", ENDITEM, 
+		"Name=oldCCOption", "Value=", ENDITEM, 
+		"Name=numPassengers", "Value=1", ENDITEM, 
+		"Name=seatType", "Value={ParamSet_1}", ENDITEM, 
+		"Name=seatPref", "Value={NewParam}", ENDITEM, 
+		"Name=outboundFlight", "Value={ID}", ENDITEM, 
+		"Name=advanceDiscount", "Value=0", ENDITEM, 
+		"Name=returnFlight", "Value={ID2}", ENDITEM, 
+		"Name=JSFormSubmit", "Value=off", ENDITEM, 
+		"Name=.cgifields", "Value=saveCC", ENDITEM, 
+		"Name=buyFlights.x", "Value=42", ENDITEM, 
+		"Name=buyFlights.y", "Value=9", ENDITEM, 
+		LAST);
+
+	lr_end_transaction("2_transaction",LR_AUTO);
+
+	lr_think_time(13);
+
+	web_url("Itinerary Button", 
+		"URL=http://127.0.0.1:1080/cgi-bin/welcome.pl?page=itinerary", 
+		"TargetFrame=body", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://127.0.0.1:1080/cgi-bin/nav.pl?page=menu&in=flights", 
+		"Snapshot=t7.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_think_time(4);
+
+	lr_start_transaction("3_transaction");
+
+	web_url("Home Button", 
+		"URL=http://127.0.0.1:1080/cgi-bin/welcome.pl?page=menus", 
+		"TargetFrame=body", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://127.0.0.1:1080/cgi-bin/nav.pl?page=menu&in=itinerary", 
+		"Snapshot=t8.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_end_transaction("3_transaction",LR_AUTO);
+
+	return 0;
+}
+vuser_end()
+{
+	#include "LogQut.h"
+	
+	return 0;
+}
